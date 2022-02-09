991ffd1b23c51c0d6d081dd972eed8f315d43524
Joel <jose.varona05@gmail.com>
2022-02-09 08:30:32 +0100

    migrate logic for monitoring to this project

---
+++ src/configuration/addresses/index.ts
@@ -0,0 +1,9 @@
+import pancakeMainnet from './pancake-mainnet.json';
+import bakeryMainnet from './bakery-mainnet.json';
+import apeMainnet from './ape-mainnet.json';
+
+export default {
+  pancake: pancakeMainnet,
+  bakery: bakeryMainnet,
+  ape: apeMainnet,
+};
--- src/monitoring/monitoring.service.ts
+++ src/monitoring/monitoring.service.ts
@@ -1,12 +1,180 @@
 import { Injectable } from '@nestjs/common';
-import web3 from 'web3';
-import bigNumber from 'big-number';
+import Web3 from 'web3';
+import BigNumber from 'big-number';
 import abis from '../configuration/abis';
 import addresses from '../configuration/addresses';
+import erc20 from '../configuration/abis/erc20.json';
 
 @Injectable()
 export class MonitoringService {
-  start() {
-    console.log('start', abis.apeFactory.apeFactory[0]);
+  async start() {
+    const web3 = new Web3(
+      new Web3.providers.WebsocketProvider(process.env.BSC_WSS),
+    );
+
+    // const { address: admin } = web3.eth.accounts.wallet.add(
+    //   process.env.PRIVATE_KEY,
+    // );
+
+    //TODO refactory error with types
+    const pancakeFactoryAbi: any = abis.pancakeFactory.pancakeFactory;
+    const pancakeRouterAbi: any = abis.pancakeRouter.pancakeRouter;
+
+    // we need pancakeSwap
+    const pancakeFactory = new web3.eth.Contract(
+      pancakeFactoryAbi,
+      addresses.pancake.factory,
+    );
+    const pancakeRouter = new web3.eth.Contract(
+      pancakeRouterAbi,
+      addresses.pancake.router,
+    );
+
+    //TODO refactory error with types
+    const bakeryFactoryAbi: any = abis.bakeryFactory.bakeryFactory;
+    const bakeryRouterAbi: any = abis.bakeryRouter.bakeryRouter;
+
+    // we need bakerySwap
+    const bakeryFactory = new web3.eth.Contract(
+      bakeryFactoryAbi,
+      addresses.bakery.factory,
+    );
+    const bakeryRouter = new web3.eth.Contract(
+      bakeryRouterAbi,
+      addresses.bakery.router,
+    );
+
+    const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+    const fromTokens = ['WBNB'];
+    const fromToken = [
+      '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // WBNB
+    ];
+    const fromTokenDecimals = [18];
+
+    const toTokens = ['USDC'];
+    const toToken = [
+      '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', // USDC
+    ];
+    const toTokenDecimals = [18];
+
+    //TODO refactory error with types
+    const erc20ABI: any = erc20;
+
+    // we need ERC20 ABI
+    const usdcToken = new web3.eth.Contract(erc20ABI, toToken[0]);
+
+    const amount = process.env.TEST_AMOUNT;
+
+    const networkId = await web3.eth.net.getId();
+
+    const subscription = web3.eth
+      .subscribe('newBlockHeaders', (error, result) => {
+        if (!error) {
+          // console.log(result);
+          return;
+        }
+        console.error(error);
+      })
+      .on('connected', (subscriptionId) => {
+        console.log(`You are connected on ${subscriptionId}`);
+      })
+      .on('data', async (block) => {
+        console.log(
+          '-------------------------------------------------------------',
+        );
+        console.log(`New block received. Block # ${block.number}`);
+        console.log(
+          `GasLimit: ${block.gasLimit} and Timestamp: ${block.timestamp}`,
+        );
+
+        for (let i = 0; i < fromTokens.length; i++) {
+          for (let j = 0; j < toTokens.length; j++) {
+            console.log(`Trading ${toTokens[j]}/${fromTokens[i]} ...`);
+            const pairAddress = await pancakeFactory.methods
+              .getPair(fromToken[i], toToken[j])
+              .call();
+            console.log(
+              `pairAddress ${toTokens[j]}/${fromTokens[i]} is ${pairAddress}`,
+            );
+
+            const unit0 = await new BigNumber(amount);
+            const amount0 = await new BigNumber(unit0).shiftedBy(
+              fromTokenDecimals[i],
+            );
+            console.log(
+              `Input amount of ${fromTokens[i]}: ${unit0.toString()}`,
+            );
+
+            // The quote currency needs to be WBNB
+            let tokenIn, tokenOut;
+            if (fromToken[i] === WBNB) {
+              tokenIn = fromToken[i];
+              tokenOut = toToken[j];
+            } else if (toToken[j] === WBNB) {
+              tokenIn = toToken[j];
+              tokenOut = fromToken[i];
+            } else {
+              return;
+            }
+
+            // The quote currency is not WBNB
+            if (typeof tokenIn === 'undefined') {
+              return;
+            }
+
+            // call getAmountsOut in PancakeSwap
+            const amounts = await pancakeRouter.methods
+              .getAmountsOut(amount0, [tokenIn, tokenOut])
+              .call();
+            const unit1 = await new BigNumber(amounts[1]).shiftedBy(
+              -toTokenDecimals[j],
+            );
+            const amount1 = await new BigNumber(amounts[1]);
+            console.log(`
+                    Buying token at PancakeSwap DEX
+                    =================
+                    tokenIn: ${unit0.toString()} ${fromTokens[i]}
+                    tokenOut: ${unit1.toString()} ${toTokens[j]}
+                `);
+
+            // call getAmountsOut in BakerySwap
+            const amounts2 = await bakeryRouter.methods
+              .getAmountsOut(amount1, [tokenOut, tokenIn])
+              .call();
+            const unit2 = await new BigNumber(amounts2[1]).shiftedBy(
+              -fromTokenDecimals[i],
+            );
+            const amount2 = await new BigNumber(amounts2[1]);
+            console.log(`
+                    Buying back token at BakerySwap DEX
+                    =================
+                    tokenOut: ${unit1.toString()} ${toTokens[j]}
+                    tokenIn: ${unit2.toString()} ${fromTokens[i]}
+                `);
+
+            const profit = await new BigNumber(amount2).minus(amount0);
+            const unit3 = await new BigNumber(unit2).minus(unit0);
+            // not consider transaction cost in here
+            console.log(`Profit in ${fromTokens[i]}: ${unit3.toString()}`);
+
+            if (profit > 0) {
+              console.log(`
+                        Block # ${block.number}: Arbitrage opportunity found!
+                        Expected profit: ${unit3.toString()} in ${fromTokens[i]}
+                    `);
+            } else {
+              console.log(`
+                        Block # ${
+                          block.number
+                        }: Arbitrage opportunity not found!
+                        Expected profit: ${unit3.toString()} in ${fromTokens[i]}
+                    `);
+            }
+          }
+        }
+      })
+      .on('error', (error) => {
+        console.log(error);
+      });
   }
 }
